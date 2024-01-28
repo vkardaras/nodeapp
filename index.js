fa39f84fa076e2e7c3ce15bbcfdb516d3c436b1d
Vasilis Kardaras <vasiliskardaras@gmail.com>
2024-01-28 23:21:19 +0200

    add express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express  = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello There!');
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000')
+})
